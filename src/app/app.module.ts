7d3b3c3499dc41a8bd8c4fa73b45f3976f7e75e6
Apollostowel <apollostowel@icloud.com>
2022-10-28 12:59:15 +0300

    Intial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,34 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth, initializeAuth, indexedDBLocalPersistence } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { Capacitor } from '@capacitor/core';
+import { getApp } from 'firebase/app';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  entryComponents: [],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => {
+      if (Capacitor.isNativePlatform()) {
+        return initializeAuth(getApp(), {
+          persistence: indexedDBLocalPersistence
+        });
+      } else {
+        return getAuth();
+      }
+    }),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'authenticationdemo-7fad9',
+    appId: '1:938701282228:web:2f1615c921ee2dc0f8f0e1',
+    storageBucket: 'authenticationdemo-7fad9.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCwIKYe507BceIz2umsg5SgUF78lQJTzrE',
+    authDomain: 'authenticationdemo-7fad9.firebaseapp.com',
+    messagingSenderId: '938701282228',
+  },
   production: false
 };
 
